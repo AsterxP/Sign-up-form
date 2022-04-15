a14f7f7fa502d38f2f021d53406260ae4989e724
Aster3 <bmarch87@gmail.com>
2022-04-16 08:23:16 +1000

    add error to pwd

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 const pwd = document.querySelector("#pwd");
 const confirm = document.querySelector("#confirm-pwd");
-
+const container = document.querySelector("#pwd-container")
 const error = document.createElement('p');
-error.textContent = 'apple';
-pwd.appendChild(error)
 
-// if (pwd.value === confirm.value) {
-//   error.textContent='';
-//   pwd.appendChild(error)
-// } else {
-//   error.textContent='* Passwords do not match';
-//   pwd.appendChild(error)
-// }
+pwd.addEventListener('change',  (event) => {
+  if (pwd.value === confirm.value) {
+    error.textContent='';
+    container.appendChild(error)
+  } else {
+    error.textContent='*Passwords do not match';
+    container.appendChild(error)
+  }
+});
+
+confirm.addEventListener('change',  (event) => {
+  if (pwd.value === confirm.value) {
+    error.textContent='';
+    container.appendChild(error)
+  } else {
+    error.textContent='*Passwords do not match';
+    container.appendChild(error)
+  }
+});
 
